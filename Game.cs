0d7dab1692a93bb35ccab7ee7abcea9856945d05
wangqi <x913724685@live.com>
2021-08-08 22:41:54 +0800

    complete game

--- Game.cs
+++ Game.cs
@@ -2,17 +2,29 @@ using System;
 
 namespace GuessNumber
 {
+    enum CompareResult { Bigger, Smaller, Equal } //the input number is bigger, smaller or equal to the hidden number
     class Game
     {
         // The number to guess
         public int HiddenNumber { get; private set; }
-        public Game()
+        public Game(int minValue = 1, int maxValue = 100)
         {
-            HiddenNumber = 2;
+            // hidden number is bigger or equal to minValue
+            // less or equal to maxValue
+            var rand = new Random();
+            HiddenNumber = rand.Next(minValue, maxValue + 1);
         }
-        public static void hello()
+        public CompareResult Compare(int guess)
         {
-            System.Console.WriteLine("Hello Game!");
+            if (guess < HiddenNumber)
+            {
+                return CompareResult.Smaller;
+            }
+            if (guess == HiddenNumber)
+            {
+                return CompareResult.Equal;
+            }
+            return CompareResult.Bigger;
         }
     }
 }
