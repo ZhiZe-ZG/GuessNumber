9e4b1f02040297a4a80cbf0c8d4206ced429814a
wangqi <x913724685@live.com>
2021-08-10 22:10:23 +0800

    clean code

--- Program.cs
+++ Program.cs
@@ -9,34 +9,34 @@ namespace GuessNumber
         {
             using (var game1 = new GameGUI())
                 game1.Run();
-            var game = new GuessNumberGame();
-            var guessNumber=0;
-            var inputStr = "";
-            bool endGame = false;
-            Console.WriteLine("Welcome to Guess Number!");
-            while (!endGame)
-            {
-                Console.WriteLine("Please input a number (end with enter):");
-                inputStr = Console.ReadLine();
-                try
-                {
-                    guessNumber=Convert.ToInt32(inputStr);
-                }
-                catch
-                {// handle input error
-                    continue;
-                }
-                var guessResult = game.Compare(guessNumber);
-                if (guessResult is CompareResult.Equal){
-                    endGame = true;
-                    Console.WriteLine("You win!");
-                }
-                else{
-                    Console.WriteLine($"Your guess number is {guessResult} than the hidden number.");
-                }
-            }
-            Console.WriteLine("Game end.");
-            Console.ReadKey();
+            // var game = new GuessNumberGame();
+            // var guessNumber=0;
+            // var inputStr = "";
+            // bool endGame = false;
+            // Console.WriteLine("Welcome to Guess Number!");
+            // while (!endGame)
+            // {
+            //     Console.WriteLine("Please input a number (end with enter):");
+            //     inputStr = Console.ReadLine();
+            //     try
+            //     {
+            //         guessNumber=Convert.ToInt32(inputStr);
+            //     }
+            //     catch
+            //     {// handle input error
+            //         continue;
+            //     }
+            //     var guessResult = game.Compare(guessNumber);
+            //     if (guessResult is CompareResult.Equal){
+            //         endGame = true;
+            //         Console.WriteLine("You win!");
+            //     }
+            //     else{
+            //         Console.WriteLine($"Your guess number is {guessResult} than the hidden number.");
+            //     }
+            // }
+            // Console.WriteLine("Game end.");
+            // Console.ReadKey();
         }
     }
 }
